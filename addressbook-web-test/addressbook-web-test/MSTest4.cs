f1304a19abb162a3063a9aaa225264593e9671a4
VeraLev <levchenkovera@gmail.com>
2020-06-21 15:19:04 +0300

    добавила библиотеки

--- addressbook-web-test/addressbook-web-test/MSTest4.cs
+++ addressbook-web-test/addressbook-web-test/MSTest4.cs
@@ -2,63 +2,77 @@
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
-using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
-namespace addressbook_FF_test
+namespace SeleniumTests
 {
-    [TestFixture]
+    [TestClass]
     public class UntitledTestCase
     {
-        private IWebDriver driver;
+        private static IWebDriver driver;
         private StringBuilder verificationErrors;
-        private string baseURL;
+        private static string baseURL;
         private bool acceptNextAlert = true;
 
-        [SetUp]
-        public void SetupTest()
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
         {
             driver = new FirefoxDriver();
             baseURL = "http://localhost/addressbook";
-            verificationErrors = new StringBuilder();
         }
 
-        [TearDown]
-        public void TeardownTest()
+        [ClassCleanup]
+        public static void CleanupClass()
         {
             try
             {
-                driver.Quit();
+                //driver.Quit();// quit does not close the window
+                driver.Close();
+                driver.Dispose();
             }
             catch (Exception)
             {
                 // Ignore errors if unable to close the browser
             }
+        }
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            verificationErrors = new StringBuilder();
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
             Assert.AreEqual("", verificationErrors.ToString());
         }
 
-        [Test]
+        [TestMethod]
         public void TheUntitledTestCaseTest()
         {
             driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Name("user")).Click();
             driver.FindElement(By.Name("user")).Clear();
             driver.FindElement(By.Name("user")).SendKeys("admin");
+            driver.FindElement(By.Id("LoginForm")).Click();
+            driver.FindElement(By.Name("pass")).Click();
             driver.FindElement(By.Name("pass")).Clear();
             driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.Id("LoginForm")).Submit();
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
             driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.XPath("(//input[@name='new'])[2]")).Click();
+            driver.FindElement(By.Name("new")).Click();
             driver.FindElement(By.Name("group_name")).Click();
             driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("w");
-            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_name")).SendKeys("q");
             driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("w");
-            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_header")).SendKeys("q");
             driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("w");
+            driver.FindElement(By.Name("group_footer")).SendKeys("q");
+            driver.FindElement(By.XPath("//form[@action='/addressbook/group.php']")).Click();
             driver.FindElement(By.Name("submit")).Click();
             driver.FindElement(By.LinkText("groups")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
